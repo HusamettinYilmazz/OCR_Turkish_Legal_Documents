1df4a6763dcb20a744eacbfbae458233dcbb6de1
Husam <husamahmed616@gmail.com>
2026-02-17 13:21:01 +0300

    correct extensions in assets/cumhurbaskanligi_kararnameleri/  &  fix file naming in assets/cumhurbaskanligi_genelgeleri/

---
+++ scripts/file_manipulation.py
@@ -0,0 +1,24 @@
+import os
+from glob import glob
+
+def fix_files_naming(circulars_dir, decree_files):
+    circular_files = glob(os.path.join(circulars_dir, "*.pdf"))
+    for idx in range(len(circular_files)):
+        new_filename = circular_files[idx].replace("document", "")
+        os.rename(circular_files[idx], new_filename)
+
+    decree_files = glob(os.path.join(decrees_dir, "*"))
+    for idx in range(len(decree_files)):
+        ## delete .pdf if exist for consistent naming
+        decree_file_nopdf = decree_files[idx].replace(".pdf", "")
+        ## add .pdf for all files
+        new_filename = decree_file_nopdf.replace(decree_file_nopdf, f"{decree_file_nopdf}.pdf")
+        os.rename(decree_files[idx], new_filename)
+
+
+assets_dir = "/home/husammm/Desktop/courses/cs_courses/DL/projects/vlm_ocr_turkish/assets"
+circulars_dir = os.path.join(assets_dir, "cumhurbaskanligi_genelgeleri")
+decrees_dir = os.path.join(assets_dir, "cumhurbaskanligi_kararnameleri")
+
+if __name__ == "__main__":
+    fix_files_naming(circulars_dir, decrees_dir)
